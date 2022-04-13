f7f336075ff789572ad9d9d8d45fbe50b75b26dc
Sean McKinney <seanamckinney8@gmail.com>
2022-04-13 16:28:22 -0500

    initial commit.  took Fibsequence and just loged the x and used trial and error to find the first 10 digit return.

--- TenDigitFibonacci/Program.cs
+++ TenDigitFibonacci/Program.cs
@@ -10,6 +10,20 @@ namespace TenDigitFibonacci
     {
         static void Main(string[] args)
         {
-        }
-    }
-}
+           
+            int x = 0;
+            int y = 1;
+            int z = 0;
+            for (int i = 0; i < 45; i++)
+            {
+                z = x + y;
+                x = y;
+                y = z;
+               
+            Console.WriteLine(x);
+            }
+
+            Console.WriteLine("Via trial and error: 45th iteration gives x = 1,134,903,170");
+        }//end main
+    }//end class
+}//end name space
